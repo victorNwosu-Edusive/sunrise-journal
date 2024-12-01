b91c52b48c3de5e59ad1a40ec8e7284af5db2de9
Victor Nwosu <nwosuuvictor@gmail.com>
2024-12-01 12:00:18 +0100

    added a few responsiveness to loading screen

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -5,11 +5,11 @@ import logo from '../assets/images/sun-favicon.png'
 const Loading = () => {
   return (
     <div className='flex h-lvh w-full justify-center items-center bg-orange-700'>
-      <img src={load} className='w-full h-full z-50 '/>
+      <img src={load} className='w-full h-full object-cover object-center z-50 '/>
       <div className='absolute flex justify-center z-50'>
       <div className='flex bg-orange-900 p-4'>
-         <img src={logo} alt="" className='h-8 w-8'  />
-         <p className='font-groteskbold text-sm text-white leading-3 pt-2'>Sunrise <br />Journal</p>
+         <img src={logo} alt="" className='h-8 w-8 md:h-14 md:w-14'  />
+         <p className='font-groteskbold text-sm md:text-xl md:leading-3 text-white leading-3 pt-2'>Sunrise <br />Journal</p>
          </div>
       </div>
     </div>
