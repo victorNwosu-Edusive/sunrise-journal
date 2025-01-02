294568b98cd1b2dbfdfd1e4f06b2ceb6aa51ca59
Victor Nwosu <nwosuuvictor@gmail.com>
2025-01-02 17:28:00 +0100

    updated login modal

--- src/components/loginmodal.jsx
+++ src/components/loginmodal.jsx
@@ -4,20 +4,16 @@ import logo from '../assets/images/sun-favicon.png'
 function Loginmodal({ onClose }) {
     return (
         <div
-            className="fixed inset-0 flex items-center justify-center backdrop-blur-sm bg-orange-950/70 z-50"
+            className="fixed inset-0 flex items-center justify-center backdrop-blur-sm bg-orange-100/35 z-50"
             onClick={onClose} // Close modal when clicking outside
         >
             <div
-                className="bg-white p-6 rounded-lg shadow-lg w-96"
+                className="bg-white p-6 rounded-lg shadow-lg w-96 mx-10"
                 onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside
             >
                 <div className='flex flex-col gap-5'>
-                <div className='flex'>
-                <img src={logo} alt="" className='h-11 w-11'  />
-                <p className='font-groteskbold text-sm text-black leading-3 pt-2'>Sunrise <br />Journal</p>
                 </div>
-                </div>
-                <h2 className="text-2xl font-groteskbold mb-4">Login</h2>
+                <h2 className="text-2xl font-bold mb-4">Login</h2>
                 <form>
                     <div className="mb-4">
                         <label className="block text-sm font-medium text-gray-700">
@@ -25,7 +21,7 @@ function Loginmodal({ onClose }) {
                         </label>
                         <input
                             type="email"
-                            className="w-full p-2 border rounded-lg outline-none focus:ring-2 focus:ring-orange-600"
+                            className="w-full p-2 border-none bg-orange-100/35 border-[1px] duration-300 rounded-md outline-none focus:ring-2 focus:ring-orange-100 text-sm"
                             placeholder="Enter your email"
                         />
                     </div>
@@ -35,13 +31,13 @@ function Loginmodal({ onClose }) {
                         </label>
                         <input
                             type="password"
-                            className="w-full p-2 border rounded-lg outline-none focus:ring-2 focus:ring-orange-600"
+                            className="w-full p-2 border-none bg-orange-100/35 border-[1px] duration-300 rounded-md outline-none focus:ring-2 focus:ring-orange-100 text-sm"
                             placeholder="Enter your password"
                         />
                     </div>
                     <button
                         type="submit"
-                        className="w-full bg-orange-900 text-white p-2 rounded-lg hover:bg-orange-700 transition duration-300"
+                        className="w-full bg-orange-600/55 text-white font-bold p-2 rounded-lg hover:bg-orange-700 transition duration-300"
                     >
                         Login
                     </button>
