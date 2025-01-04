0d40fa5e858fa7c550c84ffc2812d7068fa7a4e6
Victor Nwosu <nwosuuvictor@gmail.com>
2025-01-04 01:30:42 +0100

    corrections

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -9,13 +9,13 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB7dZFsSDtg86X89LbevsbUyoydNuHS85E",
-  authDomain: "sunrise-journal-9c998.firebaseapp.com",
-  projectId: "sunrise-journal-9c998",
-  storageBucket: "sunrise-journal-9c998.firebasestorage.app",
-  messagingSenderId: "574648221945",
-  appId: "1:574648221945:web:e505e11662d6ec0d3de228",
-  measurementId: "G-9611W8368Y"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
