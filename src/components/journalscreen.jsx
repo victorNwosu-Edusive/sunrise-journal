c5645035cdd959ad5175327ac27d25c093232b3d
Victor Nwosu <nwosuuvictor@gmail.com>
2025-01-21 17:10:33 +0100

    minor corrections to "journal screen"

--- src/components/journalscreen.jsx
+++ src/components/journalscreen.jsx
@@ -19,7 +19,7 @@ function Journalscreen(){
        
         <div className="h-screen md:h-screen lg:h-screen w-full md:w-full lg:w-4/5 ml-auto">
         <div className=' p-9 bg-orange-100/25 pb-24'>
-         <h1 className='font-bold text-orange-950 text-2xl md:text-4xl lg:text-4xl mt-16 md:mt-9 lg:mt-9 mb-5'>Welcome to <span className='text-orange-600/55'>Sunrise Journal</span>👋</h1>
+         <h1 className='font-bold text-orange-950 text-2xl md:text-4xl lg:text-4xl mt-16 md:mt-9 lg:mt-9 mb-5'>Welcome to Sunrise Journal👋</h1>
          <h1 className='text-[14px] md:text-[15px] lg:text-[15px] text-slate-600'>Sunrise Journal is a sleek and intuitive journaling app designed to help you capture your thoughts, 
             ideas, and memories effortlessly. With its elegant design and user-friendly interface, Sunrise Journal 
             makes it easy to log daily reflections, set goals, and track your personal growth. Start your day with 
