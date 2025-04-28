42abdf55bad89df563e696f5adcdca5498beee04
Victor Nwosu <nwosuuvictor@gmail.com>
2025-04-28 23:26:41 +0100

    commits VI

--- src/components/journalscreen.jsx
+++ src/components/journalscreen.jsx
@@ -34,8 +34,8 @@ function Journalscreen(){
         <div className='rounded-xl p-5 m-6 md:m-16 lg:m-16 -mt-32 md:-mt-32 lg:-mt-40 shadow-sm flex z-10 flex-col items-center justify-center'>
             <img src={heroImage} alt="" className=' mb-5 h-40 w-auto flex items-center justify-center' />
         <div className='flex gap-3'>
-        <NavLink onClick={toggleModal} className="text-white text-[12px] md:text-sm lg:text-sm bg-gradient-to-b from-orange-400/65 to-orange-600 hover:bg-orange-600/45 p-2 px-7 md:px-10 lg:px-10 font-medium duration-300 rounded-md items-center justify-center"> Sign in</NavLink>
-        <NavLink onClick={toggleModal} className="text-slate-600 text-[12px] md:text-sm lg:text-sm bg-gradient-to-b from-slate-100 to-slate-300 p-2 px-7 md:px-10 lg:px-10 font-medium hover:bg-slate-300 duration-300 rounded-md items-center justify-center"> Sign up</NavLink>
+        <NavLink onClick={toggleModal} className="text-white text-[12px] md:text-sm lg:text-sm bg-gradient-to-b from-orange-400/55 to-orange-600 hover:bg-orange-600/45 p-2 px-7 md:px-10 lg:px-10 font-medium duration-300 rounded-md items-center justify-center"> Sign in</NavLink>
+        <NavLink onClick={toggleModal} className="text-slate-600 text-[12px] md:text-sm lg:text-sm bg-gradient-to-b from-slate-100 to-slate-200 p-2 px-7 md:px-10 lg:px-10 font-medium hover:bg-slate-300 duration-300 rounded-md items-center justify-center"> Sign up</NavLink>
         </div>    
             {isModalOpen && <Loginmodal onClose={toggleModal} />}
 
